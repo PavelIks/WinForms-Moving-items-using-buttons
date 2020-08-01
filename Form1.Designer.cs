df81bb6e249d4e8587db511fbba686356e99e1f5
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-01 14:47:49 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace MovingElements
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MaximumSize = new System.Drawing.Size(416, 338);
+            this.MinimumSize = new System.Drawing.Size(416, 338);
+            this.components = new System.ComponentModel.Container();
+            this.StartPosition = FormStartPosition.Manual;
+            //
+            // ButtonCenter
+            //
+            ButtonCenter.Location = new Point(180, 30);
+            ButtonCenter.Text = "O";
+            this.Controls.Add(ButtonCenter);
+            ButtonCenter.Click += ButtonCenter_Click;
+            ButtonCenter.MouseHover += delegate { Cursor = Cursors.SizeAll; };
+            ButtonCenter.MouseLeave += delegate { Cursor = Cursors.Default; };
+            //
+            // ButtonLeft
+            //
+            ButtonLeft.Location = new Point(100, 30);
+            ButtonLeft.Text = "<-";
+            this.Controls.Add(ButtonLeft);
+            ButtonLeft.Click += ButtonLeft_Click;
+            ButtonLeft.MouseHover += delegate { Cursor = Cursors.PanWest; };
+            ButtonLeft.MouseLeave += delegate { Cursor = Cursors.Default; };
+            ButtonLeft.LocationChanged += delegate
+            {
+                if (ButtonLeft.Location.X == 0)
+                {
+                    BackColor = Color.Orange;
+                }
+            };
+            //
+            // ButtonRight
+            //
+            ButtonRight.Location = new Point(260, 30);
+            ButtonRight.Text = "->";
+            this.Controls.Add(ButtonRight);
+            ButtonRight.Click += ButtonRight_Click;
+            ButtonRight.MouseHover += delegate { Cursor = Cursors.PanEast; };
+            ButtonRight.MouseLeave += delegate { Cursor = Cursors.Default; };
+            ButtonRight.LocationChanged += delegate
+            {
+                if (ButtonRight.Location.X + ButtonRight.Width >= ClientSize.Width)
+                {
+                    BackColor = Color.Yellow;
+                }
+            };
+
+            //
+            // ButtonUp
+            //
+            ButtonUp.Location = new Point(180, 0);
+            ButtonUp.Text = "/\\";
+            this.Controls.Add(ButtonUp);
+            ButtonUp.Click += ButtonUp_Click;
+            ButtonUp.MouseHover += delegate { Cursor = Cursors.PanNorth; };
+            ButtonUp.MouseLeave += delegate { Cursor = Cursors.Default; };
+            ButtonUp.LocationChanged += delegate
+            {
+                if (ButtonUp.Location.Y == 0)
+                {
+                    BackColor = Color.Green;
+                }
+            };
+
+            //
+            // ButtonDown
+            //
+            ButtonDown.Location = new Point(180, 60);
+            ButtonDown.Text = "\\/";
+            this.Controls.Add(ButtonDown);
+            ButtonDown.Click += ButtonDown_Click;
+            ButtonDown.MouseHover += delegate { Cursor = Cursors.PanSouth; };
+            ButtonDown.MouseLeave += delegate { Cursor = Cursors.Default; };
+            ButtonDown.LocationChanged += delegate
+            {
+                if (ButtonDown.Location.Y + ButtonDown.Height >= ClientSize.Height)
+                {
+                    BackColor = Color.Red;
+                }
+            };
+        }
+
+        private Button ButtonCenter = new Button();
+        private Button ButtonLeft = new Button();
+        private Button ButtonRight = new Button();
+        private Button ButtonUp = new Button();
+        private Button ButtonDown = new Button();
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MovingElements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCenter_Click(object sender, EventArgs e)
+        {
+            ButtonCenter.Location = new Point(180, 30);
+            ButtonLeft.Location = new Point(100, 30);
+            ButtonRight.Location = new Point(260, 30);
+            ButtonUp.Location = new Point(180, 0);
+            ButtonDown.Location = new Point(180, 60);
+        }
+
+        private void ButtonLeft_Click(object sender, EventArgs e)
+        {
+            if (ButtonLeft.Location.X != 0)
+            {
+                ButtonCenter.Location = new Point(ButtonCenter.Location.X - 1, ButtonCenter.Location.Y);
+                ButtonLeft.Location = new Point(ButtonLeft.Location.X - 1, ButtonCenter.Location.Y);
+                ButtonRight.Location = new Point(ButtonRight.Location.X - 1, ButtonCenter.Location.Y);
+                ButtonUp.Location = new Point(ButtonUp.Location.X - 1, ButtonUp.Location.Y);
+                ButtonDown.Location = new Point(ButtonDown.Location.X - 1, ButtonDown.Location.Y);
+            }
+            else
+            {
+                Location = new Point(Location.X - 1, Location.Y);
+            }
+            if (ButtonCenter.Location.X + ButtonCenter.Width / 2 == Width / 2)
+            {
+                BackColor = Color.Blue;
+            }
+        }
+
+        private void ButtonRight_Click(object sender, EventArgs e)
+        {
+            if (ButtonRight.Location.X + ButtonRight.Width < ClientSize.Width)
+            {
+                ButtonCenter.Location = new Point(ButtonCenter.Location.X + 1, ButtonCenter.Location.Y);
+                ButtonLeft.Location = new Point(ButtonLeft.Location.X + 1, ButtonCenter.Location.Y);
+                ButtonRight.Location = new Point(ButtonRight.Location.X + 1, ButtonCenter.Location.Y);
+                ButtonUp.Location = new Point(ButtonUp.Location.X + 1, ButtonUp.Location.Y);
+                ButtonDown.Location = new Point(ButtonDown.Location.X + 1, ButtonDown.Location.Y);
+            }
+            else
+            {
+                Location = new Point(Location.X + 1, Location.Y);
+            }
+        }
+
+        private void ButtonUp_Click(object sender, EventArgs e)
+        {
+
+            if (ButtonUp.Location.Y != 0)
+            {
+                ButtonCenter.Location = new Point(ButtonCenter.Location.X, ButtonCenter.Location.Y - 1);
+                ButtonLeft.Location = new Point(ButtonLeft.Location.X, ButtonCenter.Location.Y - 1);
+                ButtonRight.Location = new Point(ButtonRight.Location.X, ButtonCenter.Location.Y - 1);
+                ButtonUp.Location = new Point(ButtonUp.Location.X, ButtonUp.Location.Y - 1);
+                ButtonDown.Location = new Point(ButtonDown.Location.X, ButtonDown.Location.Y - 1);
+            }
+            else
+            {
+                Location = new Point(Location.X, Location.Y - 1);
+            }
+        }
+
+        private void ButtonDown_Click(object sender, EventArgs e)
+        {
+            if (ButtonDown.Location.Y + ButtonDown.Height < ClientSize.Height)
+            {
+                ButtonCenter.Location = new Point(ButtonCenter.Location.X, ButtonCenter.Location.Y + 1);
+                ButtonLeft.Location = new Point(ButtonLeft.Location.X, ButtonCenter.Location.Y + 1);
+                ButtonRight.Location = new Point(ButtonRight.Location.X, ButtonCenter.Location.Y + 1);
+                ButtonUp.Location = new Point(ButtonUp.Location.X, ButtonUp.Location.Y + 1);
+                ButtonDown.Location = new Point(ButtonDown.Location.X, ButtonDown.Location.Y + 1);
+
+            }
+            else
+            {
+                Location = new Point(Location.X, Location.Y + 1);
+            }
+        }
+    }
+}
